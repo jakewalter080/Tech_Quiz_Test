30e7ec38cffad548de855b95644cbb5f38bb420d
Jake Walter <jakewalter080@gmail.com>
2024-12-02 15:29:25 -0600

    add quiz start route

--- Develop/cypress/compontent/Quiz.cy.tsx
+++ Develop/cypress/compontent/Quiz.cy.tsx
@@ -10,3 +10,31 @@ describe('Quiz Component Tests', () => {
       cy.get('.btn.btn-primary').contains('Start Quiz').should('be.visible')
     })
   })
+
+  describe('Quiz Start', () => {
+    beforeEach(() => {
+      // Mock the API response with test questions
+      cy.intercept('GET', '/api/questions/random', {
+        body: [
+          {
+            _id: '1',
+            question: 'What is React?',
+            answers: [
+              { text: 'A JavaScript library', isCorrect: true },
+              { text: 'A database', isCorrect: false },
+              { text: 'A server', isCorrect: false }
+            ]
+          },
+          {
+            _id: '2',
+            question: 'What is TypeScript?',
+            answers: [
+              { text: 'A superset of JavaScript', isCorrect: true },
+              { text: 'A database', isCorrect: false }
+            ]
+          }
+        ]
+      }).as('getQuestions')
+    })
+
+    
