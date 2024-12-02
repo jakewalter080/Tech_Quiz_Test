82d5e0469b785e1ba5de3a35639609b36b7dbefe
Jake Walter <jakewalter080@gmail.com>
2024-12-02 15:35:45 -0600

    add quiz start and nav

--- Develop/cypress/e2e/quiz.cy.ts
+++ Develop/cypress/e2e/quiz.cy.ts
@@ -2,3 +2,16 @@ describe('Tech Quiz E2E Tests', () => {
     beforeEach(() => {
       cy.visit('/')
     })
+
+    describe('Quiz Start and Navigation', () => {
+        it('should start quiz when clicking start button', () => {
+          // Initial state check
+          cy.get('.btn.btn-primary').contains('Start Quiz').should('be.visible')
+          
+          // Start quiz
+          cy.get('.btn.btn-primary').contains('Start Quiz').click()
+          
+          // Verify question appears
+          cy.get('h2').should('exist')
+          cy.get('.alert.alert-secondary').should('have.length.at.least', 2)
+        })
