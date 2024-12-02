0a6360da2a0ff94ed94d5b8f786b9a31c34d3c4f
Jake Walter <jakewalter080@gmail.com>
2024-12-02 15:36:31 -0600

    add loading state

--- Develop/cypress/e2e/quiz.cy.ts
+++ Develop/cypress/e2e/quiz.cy.ts
@@ -15,3 +15,18 @@ describe('Tech Quiz E2E Tests', () => {
           cy.get('h2').should('exist')
           cy.get('.alert.alert-secondary').should('have.length.at.least', 2)
         })
+
+    it('should show loading state while fetching questions', () => {
+        cy.intercept('GET', '/api/questions/random', (req) => {
+          req.reply({
+            delay: 1000,
+            fixture: 'questions.json'
+          })
+        }).as('getQuestionsDelayed')
+  
+        cy.get('.btn.btn-primary').contains('Start Quiz').click()
+        cy.get('.spinner-border').should('be.visible')
+        cy.wait('@getQuestionsDelayed')
+        cy.get('.spinner-border').should('not.exist')
+      })
+    })
