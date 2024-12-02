db3fdf914fef6cd5c2f9e98264d29dd5205420de
Jake Walter <jakewalter080@gmail.com>
2024-12-02 15:44:03 -0600

    add questions to package.json

--- Develop/cypress/e2e/quiz.cy.ts
+++ Develop/cypress/e2e/quiz.cy.ts
@@ -5,13 +5,10 @@ describe('Tech Quiz E2E Tests', () => {
 
     describe('Quiz Start and Navigation', () => {
         it('should start quiz when clicking start button', () => {
-          // Initial state check
           cy.get('.btn.btn-primary').contains('Start Quiz').should('be.visible')
           
-          // Start quiz
           cy.get('.btn.btn-primary').contains('Start Quiz').click()
           
-          // Verify question appears
           cy.get('h2').should('exist')
           cy.get('.alert.alert-secondary').should('have.length.at.least', 2)
         })
